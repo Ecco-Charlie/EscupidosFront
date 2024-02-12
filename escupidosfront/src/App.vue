0eafdbb2bcc7419f6c13af9e95532fe3ec85e31d
Zcrafting34 <cristian.gon.bek@gmail.com>
2024-02-12 16:39:33 -0600

    Mensaje de confirmación

--- escupidosfront/src/App.vue
+++ escupidosfront/src/App.vue
@@ -1,18 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <MessageCard/> 
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+<script lang="ts" setup>
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+  import MessageCard from './components/MessageCard.vue';
 </script>
 
 <style>
